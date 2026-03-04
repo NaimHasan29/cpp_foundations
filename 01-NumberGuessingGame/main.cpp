63413d7dd6a1782be59c7a6ba60b2768a228b1e9
Naim Hasan <mmh.naim12@gmail.com>
2026-03-04 12:39:19 +0600

    feat: add input validation, game loops, and attempt limits to 01-NumberGueassingGame

--- 01-NumberGuessingGame/main.cpp
+++ 01-NumberGuessingGame/main.cpp
@@ -1,27 +1,38 @@
 #include <iostream>
-#include <ctime>   
-#include <cstdlib> 
+#include <ctime>
+#include <cstdlib>
+#include <limits>
 
 using namespace std;
 
 int main() {
-    srand(time(0)); 
-    int secretNumber = rand() % 100 + 1;
-    int guess = 0;
-    cout << "--- [Project 01: Number Guessing Game] ---" << endl;
+    char playAgain;
+    do {
+        srand(time(0));
+        int secretNumber = rand() % 100 + 1;
+        int guess = 0, attempts = 0;
+        const int MAX_ATTEMPTS = 7; 
+        
+        cout << "\nNew Game! You have " << MAX_ATTEMPTS << " tries." << endl;
 
-    while (guess != secretNumber) {
-        cout << "Enter your guess (1-100): ";
-        cin >> guess;
-
-        if (guess > secretNumber) {
-            cout << "Too high! Try again." << endl;
-        } else if (guess < secretNumber) {
-            cout << "Too low! Try again." << endl;
-        } else {
-            cout << "Congratulations! You found the number!" << endl;
+        while (guess != secretNumber && attempts < MAX_ATTEMPTS) {
+            cout << "Attempt " << attempts + 1 << ": ";
+            if (!(cin >> guess)) {
+                cin.clear(); cin.ignore(1000, '\n');
+                continue;
+            }
+            attempts++;
+            if (guess == secretNumber) cout << "Correct! Amazing!" << endl;
+            else if (guess > secretNumber) cout << "Too high!" << endl;
+            else cout << "Too low!" << endl;
         }
-    }
 
-    return 0; 
+        if (guess != secretNumber) cout << "Game Over! The number was " << secretNumber << endl;
+        
+        cout << "Play again? (y/n): ";
+        cin >> playAgain;
+    } while (playAgain == 'y' || playAgain == 'Y');
+
+    cout << "Thanks for playing, Md. Mubassirul Hasan Naim!" << endl;
+    return 0;
 }
